16c5b8b240787ecf4f0b70237ea7e25f58604801
Steven Sederburg <stevens@cloudspacestudio.com>
2024-12-17 02:09:48 -0600

    add express application into base manifest ctor

--- src/core/routing/base.manifest.ts
+++ src/core/routing/base.manifest.ts
@@ -1,7 +1,12 @@
+import * as express from 'express'
+
 import { AuthStrategy } from "../auth/authstrategy"
 import { IManifestItem } from "./imanifestitem"
 
 export class BaseManifest {
+
+    constructor(app: express.Application) {}
+
     appname: string = ''
     authStrategy: AuthStrategy = AuthStrategy.none
     dependencies: string[] = [] // sqlserver, postgresdb, mongodb 
